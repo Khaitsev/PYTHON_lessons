1f84c61f4b13ce82aa1c7f17ea1cfe248172842d
Home <khaitsev@gmail.com>
2020-04-19 21:16:55 +0300

    добавил решение в 5ом задании без sort и reverse

--- Tasks/Les2/Task_5.Les_2.py
+++ Tasks/Les2/Task_5.Les_2.py
@@ -1,11 +1,24 @@
-numbers = [7, 5, 3, 3, 2, 9]
-
+numbers = [7, 5, 3, 3, 2]
+print(numbers)
 user_nu = int(input('Введите натуральное число: '))
-
 numbers.append(user_nu)
 # еще и развернул
 numbers.reverse()
-
 numbers.sort()
+print(numbers)
+
+# или, если нельзя sort и reverse
+
+numbers = [7, 5, 3, 3, 2]
+user_nu = int(input('Введите натуральное число: '))
+print(numbers)
+print(bool(numbers.count(user_nu)))
+
+if numbers.count(user_nu) == 1:
+    numbers.insert(numbers.index(user_nu), user_nu)
+elif numbers.count(user_nu) < 1:
+    numbers.append(user_nu)
+elif numbers.count(user_nu) > 1:
+    numbers.insert((numbers.index(user_nu) - 1) + numbers.count(user_nu), user_nu)
 
 print(numbers)
