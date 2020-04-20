5111f712bec9ab96136ca68b9d986d0e0a2e3bb4
Home <khaitsev@gmail.com>
2020-04-20 16:20:18 +0300

    НУ теперь точно все работает как надо

--- Tasks/Les2/Task_5.Les_2.py
+++ Tasks/Les2/Task_5.Les_2.py
@@ -2,8 +2,7 @@ numbers = [7, 5, 3, 3, 2]
 print(numbers)
 user_nu = int(input('Введите натуральное число: '))
 numbers.append(user_nu)
-# еще и развернул
-numbers.reverse()
+
 numbers.sort()
 print(numbers)
 
@@ -16,9 +15,20 @@ print(bool(numbers.count(user_nu)))
 
 if numbers.count(user_nu) == 1:
     numbers.insert(numbers.index(user_nu), user_nu)
-elif numbers.count(user_nu) < 1:
-    numbers.append(user_nu)
+
 elif numbers.count(user_nu) > 1:
     numbers.insert((numbers.index(user_nu)) + numbers.count(user_nu), float(user_nu))
+
+elif numbers.count(user_nu) < 1:
+    for n in numbers:
+        if n == numbers[-1]:
+            numbers.append(float(user_nu))
+            break
+        elif user_nu > n:
+            numbers.insert(numbers.index(n), float(user_nu))
+        elif user_nu < n:
+            continue
+
+        break
 
 print(numbers)
